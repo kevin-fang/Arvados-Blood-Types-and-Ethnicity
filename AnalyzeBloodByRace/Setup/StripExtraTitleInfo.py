9d76c04080f396ade177698c837c67e96d9d7775
= <kfang1233@gmail.com>
2017-04-14 23:29:22 -0400

    vastly refactored code so it works better with the CSV files and reorganized file structure

---
+++ AnalyzeBloodByRace/Setup/StripExtraTitleInfo.py
@@ -0,0 +1,20 @@
+import os
+
+# loop through every file in the directory and rename if needed
+for filename in os.listdir("."):
+    if filename.startswith("hu"): # genome is from the Personal Genome Project
+        os.rename(filename, filename[0:8])
+
+    elif filename.startswith("data"): # genome is from the 1000 Genomes Project
+        firstAppearance = 0;
+        secondAppearance = 0;
+        count = 0
+        firstAlready = False;
+        for letter in filename:
+            if letter == "_" and firstAlready == False:
+                firstAppearance = count
+                firstAlready = True
+            elif letter == "_" and firstAlready == True and secondAppearance == 0:
+                secondAppearance = count
+            count += 1
+        os.rename(filename, filename[firstAppearance + 1:secondAppearance])
